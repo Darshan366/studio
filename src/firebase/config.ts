f7f4672890e6e3b6fa278beb40c8f96bb8ef4ab4
Darshan Patil <darshansapatil05@gmail.com>
2025-10-03 12:48:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6932348480-db54c",
+  "appId": "1:319348302499:web:357944862e069b04661516",
+  "apiKey": "AIzaSyDgyDh1iRJ_-ofN_ChzPVXZjxSjqB-zYtM",
+  "authDomain": "studio-6932348480-db54c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "319348302499"
+};
